1d0a345637c0e191dac1c902f230f1f7b9b85fa5
Praveenkumar Angadi <praveenkumarangadiiigmail.com>
2019-06-19 18:20:01 +0530

    added Factorial_number program

---
+++ F.java
@@ -0,0 +1,16 @@
+
+public class F {
+	
+	public static int factorial (int N) {
+		if(N<=1)
+			return 1;
+		else return(N * factorial(N-1));
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(factorial(4));
+
+	}
+
+}
